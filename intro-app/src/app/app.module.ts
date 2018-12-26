5f8df46215325c4e1f2749c4383c1663610ae99e
icarvaja <icarvaja@everis.com>
2018-12-25 21:20:29 -0300

    create components
     ng g component components/header
     ng g component components/footer
     ng g component components/contact
     ng g component components/moneyChange

--- intro-app/src/app/app.module.ts
+++ intro-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { MoneyChangeComponent } from './components/money-change/money-change.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContactComponent,
+    MoneyChangeComponent
   ],
   imports: [
     BrowserModule,
