d5599ebcfa820471205a6a2501ca633c20ff8e19
icarvaja <icarvaja@everis.com>
2018-12-26 01:15:26 -0300

    create services
    ng g service services/user/user
     ng g service services/money/money

--- intro-app/src/app/app.module.ts
+++ intro-app/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { MoneyService } from './services/money/money.service';
+import { UserService } from './services/user/user.service'
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { MoneyChangeComponent } from './components/money-change/money-change.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -23,7 +27,10 @@ import { MoneyChangeComponent } from './components/money-change/money-change.com
     AppRoutingModule,
     NgbModule
   ],
-  providers: [],
+  providers: [
+    MoneyService,
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
