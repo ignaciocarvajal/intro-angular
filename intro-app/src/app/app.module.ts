3a18f8324de1b291663281176951ccd291bff74c
icarvaja <icarvaja@everis.com>
2018-12-25 23:27:44 -0300

    step 1.5 add bootstrap
    npm install bootstrap --save
    npm install --save @ng-bootstrap/ng-bootstrap

--- intro-app/src/app/app.module.ts
+++ intro-app/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -18,7 +20,8 @@ import { MoneyChangeComponent } from './components/money-change/money-change.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
