04ee1864ac67bd25385eb2c8ee35dbde2524d0e4
icarvaja <icarvaja@everis.com>
2018-12-25 23:30:57 -0300

    step 3 add route

--- intro-app/src/app/app-routing.module.ts
+++ intro-app/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ContactComponent } from './components/contact/contact.component';
+import { MoneyChangeComponent } from './components/money-change/money-change.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/change', pathMatch: 'full' },
+  { path: 'change', component: MoneyChangeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', component: MoneyChangeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+  
+}
